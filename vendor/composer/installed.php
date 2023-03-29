0a0bc16346ee4ad634e3970fe14bb6e81c04203d
Quang Trần <quangtran97hy4@gmail.com>
2023-03-29 09:44:59 +0700

    pull

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '48ee3372ecde587384adaab1852e9cb751ed1e68',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -422,9 +422,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '48ee3372ecde587384adaab1852e9cb751ed1e68',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
