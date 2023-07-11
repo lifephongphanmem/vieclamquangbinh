89a4ff4d6b1438e3ce2019af0cde51a011453680
Your Name <you@example.com>
2023-07-11 16:28:43 +0700

    pull

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -165,10 +165,9 @@ class AdminController extends Controller
 		$ldkhongthamgia = array('kytruoc' => 0, 'kyhientai' => 0);
 
 		if (in_array(session('admin')->sadmin, ['ADMIN', 'SSA'])) {
-			$model= tonghopcunglaodong::wherein('kydieutra', [$kydieutra_truoc,$kydieutra_hientai]);
-			
-			$model_truoc = $model->where('kydieutra', $kydieutra_truoc)->get();
-			// dd($model);
+			$model= tonghopcunglaodong::wherein('kydieutra', [$kydieutra_truoc,$kydieutra_hientai])->get();
+
+			$model_truoc = $model->where('kydieutra', $kydieutra_truoc);
 			if (count($model_truoc) > 0) {
 				foreach ($model_truoc as $m_truoc) {
 					$tongsonhankhau['kytruoc'] += $m_truoc['ldtren15'];
@@ -177,7 +176,7 @@ class AdminController extends Controller
 					$ldkhongthamgia['kytruoc'] += $m_truoc['ldkhongthamgia'];
 				}
 			}
-			$model_hientai = $model->where('kydieutra', $kydieutra_hientai)->get();
+			$model_hientai = $model->where('kydieutra', $kydieutra_hientai);
 			if (count($model_hientai) > 0) {
 				foreach ($model_hientai as $m_hientai) {
 					$tongsonhankhau['kyhientai'] += $m_hientai['ldtren15'];
@@ -187,14 +186,14 @@ class AdminController extends Controller
 				}
 			}
 		} else if (session('admin')->capdo == 'H') {
-			$model= tonghopcunglaodong::wherein('kydieutra', [$kydieutra_truoc,$kydieutra_hientai]);
+			$model= tonghopcunglaodong::wherein('kydieutra', [$kydieutra_truoc,$kydieutra_hientai])->get();
 			// $madv_huyen = array_column(getMaXa(session('admin')->maquocgia)->toarray(), 'madv');
 			$dmdonvi = dmdonvi::join('danhmuchanhchinh', 'danhmuchanhchinh.id', 'dmdonvi.madiaban')
 				->select('dmdonvi.madv', 'dmdonvi.tendv', 'danhmuchanhchinh.capdo', 'danhmuchanhchinh.level', 'danhmuchanhchinh.parent', 'danhmuchanhchinh.maquocgia')
 				->get();
 			$maquocgia = $dmdonvi->where('madv', session('admin')->madv)->first()->maquocgia;
 			$ds_madv_xa = array_column($dmdonvi->where('parent', $maquocgia)->toarray(), 'madv');
-			$model_truoc = $model->where('kydieutra', $kydieutra_truoc)->whereIn('madv', $ds_madv_xa)->get();
+			$model_truoc = $model->where('kydieutra', $kydieutra_truoc)->whereIn('madv', $ds_madv_xa);
 			if (count($model_truoc) > 0) {
 				foreach ($model_truoc as $m_truoc) {
 					$tongsonhankhau['kytruoc'] += $m_truoc['ldtren15'];
@@ -203,7 +202,7 @@ class AdminController extends Controller
 					$ldkhongthamgia['kytruoc'] += $m_truoc['ldkhongthamgia'];
 				}
 			}
-			$model_hientai = $model->where('kydieutra', $kydieutra_hientai)->whereIn('madv', $ds_madv_xa)->get();
+			$model_hientai = $model->where('kydieutra', $kydieutra_hientai)->whereIn('madv', $ds_madv_xa);
 			if (count($model_hientai) > 0) {
 				foreach ($model_hientai as $m_hientai) {
 					$tongsonhankhau['kyhientai'] += $m_hientai['ldtren15'];
