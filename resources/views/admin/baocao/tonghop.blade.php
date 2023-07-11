bf5140d4e9f6db2699084231ca7a18233885f498
Nguyen Duy Dung <dunght38@gmail.com>
2023-07-11 15:37:00 +0700

    chinh sua

--- resources/views/admin/baocao/tonghop.blade.php
+++ resources/views/admin/baocao/tonghop.blade.php
@@ -1,17 +1,17 @@
 @extends('main_baocao')
 
 @section('content')
-<table id="data_header" width="96%" cellspacing="0" cellpadding="8" style="margin:0 auto 20px; text-align: center;">
-    <tr>
-        <td></td>
-        <td class="text-right" style="font-style:italic">Mẫu số 02</td>
-    </tr>
+    <table id="data_header" width="96%" cellspacing="0" cellpadding="8" style="margin:0 auto 20px; text-align: center;">
+        <tr>
+            <td></td>
+            <td class="text-right" style="font-style:italic">Mẫu số 02</td>
+        </tr>
         <tr>
             <td width="40%" style="vertical-align: top;">
 
                 <p>Tỉnh: Quảng Bình</p>
-                <p>{{$m_donvi->huyen}}</p>
-                <p>{{$m_donvi->name}}</p>
+                <p>{{ $m_donvi->huyen }}</p>
+                <p>{{ $m_donvi->name }}</p>
                 <hr style="width: 10%;vertical-align: top;  margin-top: 2px">
 
             </td>
@@ -28,41 +28,43 @@
     </table>
     <p id='data_body' style="text-align: center;font-weight: bold;font-size: 20px; text-transform: uppercase">DANH SÁCH<br>
     </p>
-   
+
     <table id="data_body2" border="1" cellspacing="0" cellpadding="0">
         <thead>
             <tr>
                 <th rowspan="2">TT</th>
                 <th rowspan="2">Họ và tên </th>
-                <th colspan="2"  style="width:10%">Ngày tháng năm sinh<br></th>
+                <th colspan="2" style="width:10%">Ngày tháng năm sinh<br></th>
                 <th rowspan="2">Số CCCD/CMND </th>
                 <th rowspan="2">Mã số BHXH/BHYT </th>
                 <th rowspan="2" style="width:10%">Nơi đăng ký thường trú </th>
                 <th rowspan="2">Nơi ở hiện nay</th>
-                <th rowspan="2"  style="width:3%">Đối tượng ưu tiên (Ghi theo mã số từ 1-6)</th>
+                <th rowspan="2" style="width:3%">Đối tượng ưu tiên (Ghi theo mã số từ 1-6)</th>
                 <th rowspan="2">Tên dân tộc thiểu số</th>
-                <th rowspan="2" style="width:3%">Trình độ</br>GDPT</br>cao nhất</br>đạt được</br>(Ghi theo</br>mã số từ</br>1-4)</th>
-                <th rowspan="2"  style="width:3%">Trình độ CMKT cao nhất (Ghi theo mã số từ 1-8)</th>
+                <th rowspan="2" style="width:3%">Trình độ</br>GDPT</br>cao nhất</br>đạt được</br>(Ghi theo</br>mã số
+                    từ</br>1-4)</th>
+                <th rowspan="2" style="width:3%">Trình độ CMKT cao nhất (Ghi theo mã số từ 1-8)</th>
                 <th rowspan="2">Chuyên ngành đào tạo (Ghi rõ)</th>
-                <th rowspan="2"  style="width:3%">Tình trạng tham gia HĐKT (Ghi theo mã số từ 1-3)</th>
-                <th rowspan="2"  style="width:3%">Người có việc làm (Ghi theo mã số từ 1-5)</th>
+                <th rowspan="2" style="width:3%">Tình trạng tham gia HĐKT (Ghi theo mã số từ 1-3)</th>
+                <th rowspan="2" style="width:3%">Người có việc làm (Ghi theo mã số từ 1-5)</th>
                 <th rowspan="2">Công việc cụ thể đang làm (ghi rõ)</th>
-                <th rowspan="2"  style="width:3%">Tham gia BHXH (Ghi theo mã số từ 1-3)</th>
-                <th rowspan="2"  style="width:3%">HĐLĐ (Ghi theo mã số từ 1-3)</th>
+                <th rowspan="2" style="width:3%">Tham gia BHXH (Ghi theo mã số từ 1-3)</th>
+                <th rowspan="2" style="width:3%">HĐLĐ (Ghi theo mã số từ 1-3)</th>
                 <th rowspan="2">Nơi làm việc (ghi rõ tên cơ quan/đơn vị …)</th>
-                <th rowspan="2"  style="width:3%">Loại hình nơi làm việc (Ghi theo mã số từ 1-11)</th>
+                <th rowspan="2" style="width:3%">Loại hình nơi làm việc (Ghi theo mã số từ 1-11)</th>
                 <th rowspan="2">Địa chỉ nơi làm việc </th>
                 <th rowspan="2">Người thất nghiệp</th>
-                <th rowspan="2"  style="width:3%">Thời gian thất nghiệp (Ghi theo mã số từ 1-3)</th>
-                <th rowspan="2"  style="width:3%">Không tham gia HĐKT (Ghi theo mã số từ 1-5)</th>
+                <th rowspan="2" style="width:3%">Thời gian thất nghiệp (Ghi theo mã số từ 1-3)</th>
+                <th rowspan="2" style="width:3%">Không tham gia HĐKT (Ghi theo mã số từ 1-5)</th>
             </tr>
             <tr>
                 <th>Nam</th>
                 <th>Nữ</th>
             </tr>
-            <tr >
-                @for ($i=1;$i<25;$i++)
-                    <td style="font-weight:bold; text-decoration: underline;font-style: italic;text-align: center">{{$i}}</td>
+            <tr>
+                @for ($i = 1; $i < 25; $i++)
+                    <td style="font-weight:bold; text-decoration: underline;font-style: italic;text-align: center">
+                        {{ $i }}</td>
                 @endfor
             </tr>
 
