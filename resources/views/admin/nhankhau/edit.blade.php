94400522c07294310df35f2aa42365e588fd4b87
Your Name <you@example.com>
2023-08-22 11:16:10 +0700

    fixeditnhankhau

--- resources/views/admin/nhankhau/edit.blade.php
+++ resources/views/admin/nhankhau/edit.blade.php
@@ -92,22 +92,23 @@
                                     <div class="form-group">
                                         <label>Số điện thoại</label>
                                         <input type="text" name="sdt" value="{{ $ld->sdt }}"
-                                        class="form-control" required>
+                                            class="form-control" required>
                                     </div>
                                 </div>
                                 <div class="col-md-3">
                                     <div class="form-group">
                                         <label>Khu vực</label>
                                         <select class="form-control" name="khuvuc">
-                                            <option value="1" {{$ld->khuvuc==1?'selected':''}}>Thành thị</option>
-                                            <option value="2" {{$ld->khuvuc==2?'selected':''}}>Nông thôn</option>
+                                            <option value="1" {{ $ld->khuvuc == 1 ? 'selected' : '' }}>Thành thị</option>
+                                            <option value="2" {{ $ld->khuvuc == 2 ? 'selected' : '' }}>Nông thôn</option>
                                         </select>
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label>Nơi ở hiện nay</label>
-                                        <input type="text" name="diachi" value="{{$ld->diachi}}" class="form-control">
+                                        <input type="text" name="diachi" value="{{ $ld->diachi }}"
+                                            class="form-control">
                                     </div>
                                 </div>
 
@@ -177,7 +178,8 @@
                                             <option value="">---Chọn chuyên ngành đào tạo---</option>
                                             @foreach ($m_nganhnghe as $val)
                                                 <option value="{{ $val->madm }}"
-                                                    {{ $ld->chuyennganh == $val->madm ? 'selected' : '' }}>{{ $val->tendm }}
+                                                    {{ $ld->chuyennganh == $val->madm ? 'selected' : '' }}>
+                                                    {{ $val->tendm }}
                                                 </option>
                                             @endforeach
                                         </select>
@@ -188,8 +190,10 @@
                                         <label>Đối tượng tìm kiếm việc làm <span class="text-danger">*</span></label>
                                         <select name="doituongtimvieclam" class="form-control" required>
                                             <option value="">---Chọn đối tượng tìm kiếm việc làm---</option>
-                                            <option value="1" {{$ld->doituongtimvieclam==1?'selected':''}}>Chưa từng làm việc</option>
-                                            <option value="2" {{$ld->doituongtimvieclam==2?'selected':''}}>Đã từng làm việc</option>
+                                            <option value="1" {{ $ld->doituongtimvieclam == 1 ? 'selected' : '' }}>Chưa
+                                                từng làm việc</option>
+                                            <option value="2" {{ $ld->doituongtimvieclam == 2 ? 'selected' : '' }}>Đã từng
+                                                làm việc</option>
                                         </select>
                                     </div>
                                 </div>
@@ -198,8 +202,10 @@
                                         <label>Việc làm mong muốn <span class="text-danger">*</span></label>
                                         <select name="vieclammongmuon" class="form-control">
                                             <option value="">---Chọn việc làm mong muốn---</option>
-                                            <option value="1" {{$ld->vieclammongmuon==1?'selected':''}}>Trong tỉnh, trong nước</option>
-                                            <option value="2" {{$ld->vieclammongmuon==2?'selected':''}}>Đi làm việc ở nước ngoài</option>
+                                            <option value="1" {{ $ld->vieclammongmuon == 1 ? 'selected' : '' }}>Trong tỉnh,
+                                                trong nước</option>
+                                            <option value="2" {{ $ld->vieclammongmuon == 2 ? 'selected' : '' }}>Đi làm việc
+                                                ở nước ngoài</option>
                                         </select>
                                     </div>
                                 </div>
@@ -209,7 +215,9 @@
                                         <select name="nganhnghemongmuon" class="form-control ">
                                             <option value="">---Chọn ngành nghề mong muốn---</option>
                                             @foreach ($m_nganhnghe as $val)
-                                                <option value="{{$val->madm}}" {{$ld->nganhnghemongmuon==$val->madm?'selected':''}}>{{$val->tendm}}</option>
+                                                <option value="{{ $val->madm }}"
+                                                    {{ $ld->nganhnghemongmuon == $val->madm ? 'selected' : '' }}>
+                                                    {{ $val->tendm }}</option>
                                             @endforeach
                                         </select>
                                     </div>
@@ -217,44 +225,67 @@
 
                             </div>
                             <div class="row">
-                                <div class="col-md-3">
-                                    <div class="form-group">
-                                        <label>Ngành nghề muốn học <span class="text-danger">*</span></label>
-                                        <select name="nganhnghemuonhoc" class="form-control">
-                                            <option value="">---Chọn ngành nghề muốn học---</option>
-                                            @foreach ($m_nganhnghe as $val)
-                                                <option value="{{$val->madm}}" {{$ld->nganhnghemuonhoc==$val->madm?'selected':''}}>{{$val->tendm}}</option>
-                                            @endforeach
-                                        </select>
+                                <div class="row">
+                                    <div class="col-md-3">
+                                        <div class="form-group">
+                                            <label>Thị trường </label>
+                                            <select name="thitruonglamviec" class="form-control selec2basic">
+                                                <option value="">---Chọn thị trường muốn làm việc--</option>
+                                                @foreach (getthitruong() as $k => $val)
+                                                    <option value="{{$k}}" {{ $ld->thitruonglamviec == $k ? 'selected' : '' }}>{{ $val }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="form-group">
+                                            <label>Ngành nghề muốn học <span class="text-danger">*</span></label>
+                                            <select name="nganhnghemuonhoc" class="form-control">
+                                                <option value="">---Chọn ngành nghề muốn học---</option>
+                                                @foreach ($m_nganhnghe as $val)
+                                                    <option value="{{ $val->madm }}"
+                                                        {{ $ld->nganhnghemuonhoc == $val->madm ? 'selected' : '' }}>
+                                                        {{ $val->tendm }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="form-group">
+                                            <label>Trình độ chuyên môn muốn học <span class="text-danger">*</span></label>
+                                            <select name="trinhdochuyenmonmuonhoc" class="form-control selec2basic">
+                                                <option value="">---Chọn trình độ chuyên môn muốn học---</option>
+                                                <option value="1"
+                                                    {{ $ld->trinhdochuyenmonmuonhoc == 1 ? 'selected' : '' }}>Sơ cấp</option>
+                                                <option value="2"
+                                                    {{ $ld->trinhdochuyenmonmuonhoc == 2 ? 'selected' : '' }}>Trung cấp</option>
+                                                <option value="3"
+                                                    {{ $ld->trinhdochuyenmonmuonhoc == 3 ? 'selected' : '' }}>Cao đẳng</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="form-group">
+                                            <label>Lý do thêm<span class="text-danger">*</span> </label>
+                                            <input name="lydo" class="form-control" value="{{$ld->lydo }}">
+                                        </div>
                                     </div>
                                 </div>
-                                <div class="col-md-3">
-                                    <div class="form-group">
-                                        <label>Trình độ chuyên môn muốn học <span class="text-danger">*</span></label>
-                                        <select name="trinhdochuyenmonmuonhoc" class="form-control selec2basic">
-                                            <option value="">---Chọn trình độ chuyên môn muốn học---</option>
-                                            <option value="1" {{$ld->trinhdochuyenmonmuonhoc==1?'selected':''}}>Sơ cấp</option>
-                                            <option value="2" {{$ld->trinhdochuyenmonmuonhoc==2?'selected':''}}>Trung cấp</option>
-                                            <option value="3" {{$ld->trinhdochuyenmonmuonhoc==3?'selected':''}}>Cao đẳng</option>
-                                        </select>
+
+
+
+                                <input type="hidden" name=view value="{{ $inputs['view'] }}">
+                                <div class="form-actions">
+                                    <div class="row">
+                                        <div class="col-md-offset-4 col-md-12 text-center">
+                                            @if (chkPhanQuyen('danhsachdieutra', 'thaydoi') && $inputs['kydieutra'] == date('Y'))
+                                                <button type="submit" class="btn btn-success">Đồng ý</button>
+                                            @endif
+                                            <a onclick="history.back()" class="btn btn-danger"><i
+                                                    class="fa fa-reply"></i>&nbsp;Quay lại</a>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-
-
-
-                        <input type="hidden" name=view value="{{ $inputs['view'] }}">
-                        <div class="form-actions">
-                            <div class="row">
-                                <div class="col-md-offset-4 col-md-12 text-center">
-                                    @if (chkPhanQuyen('danhsachdieutra', 'thaydoi') && $inputs['kydieutra'] == date('Y'))
-                                        <button type="submit" class="btn btn-success">Đồng ý</button>
-                                    @endif
-                                    <a onclick="history.back()" class="btn btn-danger"><i
-                                            class="fa fa-reply"></i>&nbsp;Quay lại</a>
-                                </div>
-                            </div>
-                        </div>
                     </form>
 
                 </div>
